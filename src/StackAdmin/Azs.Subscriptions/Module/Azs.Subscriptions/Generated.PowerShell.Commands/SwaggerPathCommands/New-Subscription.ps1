ba3514291b598c67a196bc427cefbef6043e6e78
Bala Ganapathy <bganapa@microsoft.com>
2018-02-23 13:56:34 -0800

    azs user subscriptions module

---
+++ src/StackAdmin/Azs.Subscriptions/Module/Azs.Subscriptions/Generated.PowerShell.Commands/SwaggerPathCommands/New-Subscription.ps1
@@ -0,0 +1,85 @@
+<#
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License. See License.txt in the project root for license information.
+#>
+
+<#
+.SYNOPSIS
+    
+
+.DESCRIPTION
+    Create or updates a subscription.
+
+.PARAMETER SubscriptionId
+    Id of the subscription.
+
+.PARAMETER NewSubscription
+    Subscription parameter.
+
+#>
+function New-Subscription
+{
+    [OutputType([Microsoft.AzureStack.Management.Subscriptions.Models.Subscription])]
+    [CmdletBinding(DefaultParameterSetName='Subscriptions_CreateOrUpdate')]
+    param(    
+        [Parameter(Mandatory = $true, ParameterSetName = 'Subscriptions_CreateOrUpdate')]
+        [System.String]
+        $SubscriptionId,
+    
+        [Parameter(Mandatory = $true, ParameterSetName = 'Subscriptions_CreateOrUpdate')]
+        [Microsoft.AzureStack.Management.Subscriptions.Models.Subscription]
+        $NewSubscription
+    )
+
+    Begin 
+    {
+	    Initialize-PSSwaggerDependencies -Azure
+        $tracerObject = $null
+        if (('continue' -eq $DebugPreference) -or ('inquire' -eq $DebugPreference)) {
+            $oldDebugPreference = $global:DebugPreference
+			$global:DebugPreference = "continue"
+            $tracerObject = New-PSSwaggerClientTracing
+            Register-PSSwaggerClientTracing -TracerObject $tracerObject
+        }
+	}
+
+    Process {
+    
+    $ErrorActionPreference = 'Stop'
+
+    $NewServiceClient_params = @{
+        FullClientTypeName = 'Microsoft.AzureStack.Management.Subscriptions.SubscriptionsManagementClient'
+    }
+
+    $GlobalParameterHashtable = @{}
+    $NewServiceClient_params['GlobalParameterHashtable'] = $GlobalParameterHashtable
+
+    $SubscriptionsManagementClient = New-ServiceClient @NewServiceClient_params
+
+
+    if ('Subscriptions_CreateOrUpdate' -eq $PsCmdlet.ParameterSetName) {
+        Write-Verbose -Message 'Performing operation CreateOrUpdateWithHttpMessagesAsync on $SubscriptionsManagementClient.'
+        $TaskResult = $SubscriptionsManagementClient.Subscriptions.CreateOrUpdateWithHttpMessagesAsync($SubscriptionId, $NewSubscription)
+    } else {
+        Write-Verbose -Message 'Failed to map parameter set to operation method.'
+        throw 'Module failed to find operation to execute.'
+    }
+
+    if ($TaskResult) {
+        $GetTaskResult_params = @{
+            TaskResult = $TaskResult
+        }
+            
+        Get-TaskResult @GetTaskResult_params
+        
+    }
+    }
+
+    End {
+        if ($tracerObject) {
+            $global:DebugPreference = $oldDebugPreference
+            Unregister-PSSwaggerClientTracing -TracerObject $tracerObject
+        }
+    }
+}
+
