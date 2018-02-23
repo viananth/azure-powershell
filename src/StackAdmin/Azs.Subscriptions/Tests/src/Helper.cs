11c93e0e79176df1a2b086100a1d0fb195803d47
Bala Ganapathy <bganapa@microsoft.com>
2018-02-23 14:08:14 -0800

    azs user subscriptions module-test changes

--- src/StackAdmin/Azs.Subscriptions/Tests/src/Helper.cs
+++ src/StackAdmin/Azs.Subscriptions/Tests/src/Helper.cs
@@ -12,29 +12,30 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
-using System.Management.Automation;
-using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
-using Microsoft.AzureStack.Management.Gallery.Admin;
+//using System.Management.Automation;
+//using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+//using Microsoft.AzureStack.Management.Subscriptions;
 
-namespace Test
-{
-    [Cmdlet(VerbsCommon.Get, "MockClient"), OutputType(typeof(GalleryAdminClient))]
-    public class Helper : PSCmdlet {
+//namespace Test
+//{
+//    [Cmdlet(VerbsCommon.Get, "MockClient"), OutputType(typeof(SubscriptionsManagementClient))]
+//    public class Helper : PSCmdlet {
 
-        [Parameter(Mandatory = true, Position = 0, HelpMessage = "The name of your test class.")]
-        public System.String ClassName { get; set; }
+//        [Parameter(Mandatory = true, Position = 0, HelpMessage = "The name of your test class.")]
+//        public System.String ClassName { get; set; }
 
-        [Parameter(Mandatory = true, Position = 1, HelpMessage = "The name of your test.")]
-        public System.String TestName { get; set; }
+//        [Parameter(Mandatory = true, Position = 1, HelpMessage = "The name of your test.")]
+//        public System.String TestName { get; set; }
 
-        protected override void ProcessRecord() {
-            System.IO.Directory.SetCurrentDirectory(this.SessionState.Path.CurrentFileSystemLocation.ToString());
+//        protected override void ProcessRecord()
+//        {
+//            System.IO.Directory.SetCurrentDirectory(this.SessionState.Path.CurrentFileSystemLocation.ToString());
 
-            var handler = new RecordedDelegatingHandler { StatusCodeToReturn = System.Net.HttpStatusCode.OK };
-            handler.IsPassThrough = true;
+//            var handler = new RecordedDelegatingHandler { StatusCodeToReturn = System.Net.HttpStatusCode.OK };
+//            handler.IsPassThrough = true;
 
-            var context = MockContext.Start(ClassName, TestName);
-            WriteObject(context.GetServiceClient<GalleryAdminClient>(handlers: handler));
-        }
-    }
-}
+//            var context = MockContext.Start(ClassName, TestName);
+//            WriteObject(context.GetServiceClient<SubscriptionsManagementClient>(handlers: handler));
+//        }
+//    }
+//}
