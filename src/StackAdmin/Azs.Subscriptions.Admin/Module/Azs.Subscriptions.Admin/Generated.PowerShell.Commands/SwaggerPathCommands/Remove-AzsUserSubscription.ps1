56727f0f03f20bf5cbb11c362917323789f4a769
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-03-13 10:26:45 -0700

    Added GUID checking for subscriptions

--- src/StackAdmin/Azs.Subscriptions.Admin/Module/Azs.Subscriptions.Admin/Generated.PowerShell.Commands/SwaggerPathCommands/Remove-AzsUserSubscription.ps1
+++ src/StackAdmin/Azs.Subscriptions.Admin/Module/Azs.Subscriptions.Admin/Generated.PowerShell.Commands/SwaggerPathCommands/Remove-AzsUserSubscription.ps1
@@ -16,62 +16,55 @@ Licensed under the MIT License. See License.txt in the project root for license 
 .EXAMPLE
     Remove-AzsUserSubscription -SubscriptionId "c90173b1-de7a-4b1d-8600-b832b0e65946"
 #>
-function Remove-AzsUserSubscription
-{
-    [CmdletBinding(DefaultParameterSetName='Subscriptions_Delete')]
-    param(    
-        [Parameter(Mandatory = $true, ParameterSetName = 'Subscriptions_Delete', Position=0)]
-        [System.String]
+function Remove-AzsUserSubscription {
+    [CmdletBinding(DefaultParameterSetName = 'Subscriptions_Delete')]
+    param(
+        [Parameter(Mandatory = $true, ParameterSetName = 'Subscriptions_Delete', Position = 0)]
+        [ValidateNotNull()]
+        [System.Guid]
         $SubscriptionId
     )
 
-    Begin 
-    {
-	    Initialize-PSSwaggerDependencies -Azure
+    Begin {
+        Initialize-PSSwaggerDependencies -Azure
         $tracerObject = $null
         if (('continue' -eq $DebugPreference) -or ('inquire' -eq $DebugPreference)) {
             $oldDebugPreference = $global:DebugPreference
-			$global:DebugPreference = "continue"
+            $global:DebugPreference = "continue"
             $tracerObject = New-PSSwaggerClientTracing
             Register-PSSwaggerClientTracing -TracerObject $tracerObject
         }
-	}
+    }
 
     Process {
-    
-    $ErrorActionPreference = 'Stop'
 
-    $NewServiceClient_params = @{
-        FullClientTypeName = 'Microsoft.AzureStack.Management.Subscriptions.Admin.SubscriptionsAdminClient'
-    }
+        $ErrorActionPreference = 'Stop'
 
-    $GlobalParameterHashtable = @{}
-    $NewServiceClient_params['GlobalParameterHashtable'] = $GlobalParameterHashtable
-     
-    #$GlobalParameterHashtable['SubscriptionId'] = $null
-    #if($PSBoundParameters.ContainsKey('SubscriptionId')) {
-    #    $GlobalParameterHashtable['SubscriptionId'] = $PSBoundParameters['SubscriptionId']
-    #}
-
-    $SubscriptionsAdminClient = New-ServiceClient @NewServiceClient_params
-
-
-    if ('Subscriptions_Delete' -eq $PsCmdlet.ParameterSetName) {
-        Write-Verbose -Message 'Performing operation DeleteWithHttpMessagesAsync on $SubscriptionsAdminClient.'
-        $TaskResult = $SubscriptionsAdminClient.Subscriptions.DeleteWithHttpMessagesAsync($SubscriptionId)
-    } else {
-        Write-Verbose -Message 'Failed to map parameter set to operation method.'
-        throw 'Module failed to find operation to execute.'
-    }
-
-    if ($TaskResult) {
-        $GetTaskResult_params = @{
-            TaskResult = $TaskResult
+        $NewServiceClient_params = @{
+            FullClientTypeName = 'Microsoft.AzureStack.Management.Subscriptions.Admin.SubscriptionsAdminClient'
         }
-            
-        Get-TaskResult @GetTaskResult_params
-        
-    }
+
+        $GlobalParameterHashtable = @{}
+        $NewServiceClient_params['GlobalParameterHashtable'] = $GlobalParameterHashtable
+        $SubscriptionsAdminClient = New-ServiceClient @NewServiceClient_params
+
+
+        if ('Subscriptions_Delete' -eq $PsCmdlet.ParameterSetName) {
+            Write-Verbose -Message 'Performing operation DeleteWithHttpMessagesAsync on $SubscriptionsAdminClient.'
+            $TaskResult = $SubscriptionsAdminClient.Subscriptions.DeleteWithHttpMessagesAsync($SubscriptionId.ToString())
+        } else {
+            Write-Verbose -Message 'Failed to map parameter set to operation method.'
+            throw 'Module failed to find operation to execute.'
+        }
+
+        if ($TaskResult) {
+            $GetTaskResult_params = @{
+                TaskResult = $TaskResult
+            }
+
+            Get-TaskResult @GetTaskResult_params
+
+        }
     }
 
     End {
