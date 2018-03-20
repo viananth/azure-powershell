2e570007b894a26945c8c17163ae7f6361ed662b
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-03-20 13:53:54 -0700

    Added Session files

--- src/StackAdmin/Azs.InfrastructureInsights.Admin/Tests/src/Common.ps1
+++ src/StackAdmin/Azs.InfrastructureInsights.Admin/Tests/src/Common.ps1
@@ -12,7 +12,7 @@
 # limitations under the License.
 # ----------------------------------------------------------------------------------
 
-$global:ResourceGroup = "System.local"
+$global:ResourceGroupName = "System.local"
 $global:Location = "local"
 $global:TestName = ""
 
